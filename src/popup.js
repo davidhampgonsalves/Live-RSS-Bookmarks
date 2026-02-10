f2aef1c2a557446d67c5f4626547ed433a4f9aa2
tino-247 <tino-247@users.noreply.github.com>
2026-02-10 18:39:48 +0100

    Update popup.js

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,6 @@
 function setupButton(btn, func) {
   btn.addEventListener('click', async () => {
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    if (tab.url?.startsWith("chrome://")) return undefined;
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       func: func
